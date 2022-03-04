031b66c6ff9ac1ef0d98651af966e6cdee2953b3
John4312 <94133325+John4312@users.noreply.github.com>
2022-03-04 13:09:17 +0100

    Update lolbeans.lua

--- lolbeans.lua
+++ lolbeans.lua
@@ -226,7 +226,7 @@ kingpart.Name = "King Beetle Part"
 kingpart.Anchored = true
 kingpart.Transparency = 1
 kingpart.Size = Vector3.new(10, 1, 10)
-kingpart.Position = Vector3.new(175.0200653076172, 24.484851837158203, 161.5023193359375)
+kingpart.Position = Vector3.new(111.35742950439453, 10.547220230102539, 250.4386444091797)
 
 -- antfarm
 
@@ -815,7 +815,7 @@ farmt:CreateToggle("Auto Honey Wreath", nil, function(State) kocmoc.toggles.auto
 
 local mobkill = combtab:CreateSection("Combat")
 mobkill:CreateToggle("Train Tunnel Bear", nil, function(State) if State then api.humanoidrootpart().CFrame = CFrame.new(383.9188232421875, 28.60200309753418, -44.653629302978516) end end)
-mobkill:CreateToggle("Train King Beetle", nil, function(State) if State then api.humanoidrootpart().CFrame = CFrame.new(175.0200653076172, 24.484851837158203, 161.5023193359375) end end)
+mobkill:CreateToggle("Train King Beetle", nil, function(State) if State then api.humanoidrootpart().CFrame = CFrame.new(111.35742950439453, 10.547220230102539, 250.4386444091797) end end)
 mobkill:CreateToggle("Train Crab", nil, function(State) if State then api.humanoidrootpart().CFrame = CFrame.new(-307.52117919922, 107.91863250732, 467.86791992188) end end)
 mobkill:CreateToggle("Train Snail", nil, function(State) fd = game.Workspace.FlowerZones['Stump Field'] if State then api.humanoidrootpart().CFrame = CFrame.new(fd.Position.X, fd.Position.Y-6, fd.Position.Z) else api.humanoidrootpart().CFrame = CFrame.new(fd.Position.X, fd.Position.Y+2, fd.Position.Z) end end)
 mobkill:CreateToggle("Kill Mondo", nil, function(State) kocmoc.toggles.killmondo = State end)
